c0b23b6fe8cc2370bac6c0ec5ed0d7edbf438665
DZ <48256568+dzintt@users.noreply.github.com>
2021-09-07 22:45:16 -0700

    added delete

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class CreateVCC:
 
         page = await self.browser.pages()
         page = page[0]
-
         await page.goto("https://online.citi.com/US/ag/repcard/van2", {'waitUntil':'networkidle2'})
         await sendKeys(page, '//*[@id="username"]', self.user, 10000)
         await asyncio.sleep(0.5)
@@ -27,7 +26,7 @@ class CreateVCC:
         await click(page, '//*[@id="signInBtn"]', 10000)
 
         url = await page.evaluate("window.location.href")
-        while not url.startswith("https://online.citi.com/US/ag/repcard/van2/dashboard"):
+        while not url.startswith("https://online.citi.com/US/ag/repcard/van2"):
             await page.waitForNavigation({'waitUntil':'load', 'timeout': 0})
             url = await page.evaluate("window.location.href")
 
@@ -87,6 +86,51 @@ class CreateVCC:
         print(f"Task Finished")
         await queue.task_done()
 
+class DeleteVCC:
+    def __init__(self, username, password):
+        self.deleted = 0
+        self.user = username
+        self.pw = password
+        ctypes.windll.kernel32.SetConsoleTitleW(f"Citi VCC Generator ~ By @dzintt | Deleting VCC's ({self.deleted})")
+        asyncio.run(self.initBrowser())
+    
+    async def initBrowser(self):
+        self.browser = await launch({'args': ['--window-size=1600,1000'], 'headless': False , 'defaultViewport': None}, handleSIGINT=False, handleSIGTERM=False, handleSIGHUP=False, logLevel=0, autoClose=False, executablePath=chromePath)
+
+        page = await self.browser.pages()
+        page = page[0]
+
+        await page.goto("https://online.citi.com/US/ag/repcard/van2", {'waitUntil':'networkidle2'})
+        await sendKeys(page, '//*[@id="username"]', self.user, 10000)
+        await asyncio.sleep(0.5)
+        await sendKeys(page, '//*[@id="password"]', self.pw, 10000)
+        await asyncio.sleep(0.5)
+        await click(page, '//*[@id="signInBtn"]', 10000)
+
+        url = await page.evaluate("window.location.href")
+        while not url.startswith("https://online.citi.com/US/ag/repcard/van2"):
+            await page.waitForNavigation({'waitUntil':'load', 'timeout': 0})
+            url = await page.evaluate("window.location.href")
+
+        await asyncio.sleep(2)
+
+        if "dashboard" in await page.evaluate("window.location.href"):
+            while True:
+                try:
+                    await page.waitForXPath('//*[@datatarget="#deactivateModal"]', {"timeout": 5000}):
+                    await click(page, '//*[@datatarget="#deactivateModal"]', 2222)
+                    await click(page, '//*[@class="btn btn-primary"]', 2222)
+                    self.deleted += 1
+                    print(f"Deleted VCC ({self.deleted})")
+                    ctypes.windll.kernel32.SetConsoleTitleW(f"Citi VCC Generator ~ By @dzintt | Deleting VCC's ({self.deleted})")
+                    await page.reload({"waitUntil": "load"})
+                except:
+                    print("No VCC's Available to Delete")
+                    await page.close()
+                    await self.browser.close()
+        else:
+            print("No VCC's Available to Delete")
+
 async def click(page, xpath, time):
     page.waitForXPath(xpath, timeout=time)
     result = await page.Jx(xpath)
@@ -119,6 +163,7 @@ def main():
         input("Completed. Press ENTER to exit.")
         sys.exit()
     elif mode == 2:
+        DeleteVCC(username, password)
         input("Completed. Press ENTER to exit.")
         sys.exit()
     else:
